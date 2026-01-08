b80d0be07fb33295f698dc7399c9c15738954c34
danikchodo <01205862@pw.edu.pl>
2026-01-08 20:35:29 +0100

    gauss.c
    stworzenie logiki eliminacji gaussa(stworzenie macierzy jednostkowej,robienie jedynek na przykątnej i zer oprocz niej)

---
+++ gauss.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <macierz.h>
+
+void eliminacja_gauss(int n,double macierz[n][n+1]{
+  for(int i=0,i<n,i++){
+    for(int k=i+1,k<n,k++){
+    double coef=macierz[k][i]/macierz[i][i];
+    for(int j=1,j<=n,j++){
+    macierz[k][j]-=coef*macierz[i][j];
+    }
+}
+printf("krok %d\n",i+1);
+macierz_gauss(n,macierz);  
+}
+}
+  
