60220299777f97a6ace801de1e6899f3abd6e270
danikchodo <01205862@pw.edu.pl>
2026-01-09 23:10:27 +0100

    odnowienie gauss.c
    poprawa bledu z zastosowaniem pliku naglowkowego macierz.h i postawinienie "..." zamiast <...>,poprawa bledow z przecinkami w for,i poprawa bledu w ostatnim forze gdzie zamiast "i" byla napisana jedynka

--- gauss.c
+++ gauss.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <math.h>
-#include <macierz.h>
+#include "macierz.h"
 
 void eliminacja_gauss(int n,double macierz[n][n+1]{
-  for(int i=0,i<n,i++){
-    for(int k=i+1,k<n,k++){
+  for(int i=0;i<n;i++){
+    for(int k=i+1;k<n;k++){
     double coef=macierz[k][i]/macierz[i][i];
-    for(int j=1,j<=n,j++){
+    for(int j=i;j<=n;j++){
     macierz[k][j]-=coef*macierz[i][j];
     }
 }
