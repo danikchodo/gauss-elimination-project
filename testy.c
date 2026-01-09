81b2c6ab664124345a217bc55aba8cdde68c8b1a
danikchodo <01205862@pw.edu.pl>
2026-01-09 23:02:30 +0100

    odnowienie testy.c
    poprawa bledow w for(podobny z przecinkami) i tak samo brak przecinkow miedzy tablicami w tablice macierz.

--- testy.c
+++ testy.c
@@ -6,15 +6,15 @@
 int main(){
   int n=3;
 double macierz[3][4]={
-{1,1,1,6}
-{2,-1,1,3}
+{1,1,1,6},
+{2,-1,1,3},
 {1,2,-1,1}
 };
 double pierwiastki[3];
 eliminacja_gauss(n,macierz);
 pierwiastki_gauss(n,macierz,pierwiastki);
 printf("pierwiastki ukladu rownan\n");
-for(int i=0,i<n,i++){
+for(int i=0;i<n;i++){
   printf("x%d=%8.2f\n",i+1,pierwiastki[i]);}
 return 0;
 }
