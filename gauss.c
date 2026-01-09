33951fd648b0c9fcf588b13857450f6647bed4f3
danikchodo <01205862@pw.edu.pl>
2026-01-09 23:22:27 +0100

    odnowienie gauss.c
    ustawenie nawiasa po funkcji ktorego nie zauwazylem ze nie ma.

--- gauss.c
+++ gauss.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "macierz.h"
 
-void eliminacja_gauss(int n,double macierz[n][n+1]{
+void eliminacja_gauss(int n,double macierz[n][n+1]){
   for(int i=0;i<n;i++){
     for(int k=i+1;k<n;k++){
     double coef=macierz[k][i]/macierz[i][i];
