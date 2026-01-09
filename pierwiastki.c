eaa795d7bc7aac7b4264b3633b1d6ca4c6273433
danikchodo <01205862@pw.edu.pl>
2026-01-09 22:58:55 +0100

    odnowienie pierwiastki.c
    poprawione bledy w for(sredniki zamiast przecinkow)

--- pierwiastki.c
+++ pierwiastki.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void pierwiastki_gauss(int n,double macierz[n][n+1],double pierwiastki[n]){
-for(int i=n-1,i>0,i--){
+for(int i=n-1;i>0;i--){
   pierwiastki[i]=macierz[i][n];
-  for(int j=n+1,i<n,j++){
+  for(int j=n+1;i<n;j++){
   pierwiastki[i]-=macierz[i][j]*pierwiastki[j];
   }
 pierwiastki[i]/=macierz[i][i];
