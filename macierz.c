630f5f7e80f9d148dc2daa10147c2d8ba98f53ab
danikchodo <01205862@pw.edu.pl>
2026-01-09 23:06:29 +0100

    odnowienie macierz.c
    wyeleminowanie math.h za nieprzydatnoscia,poprawienie bledu z przecinkami w for.

--- macierz.c
+++ macierz.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 
 
 void macierz_gauss(int n,double macierz[n][n+1])
 {
-  for(int i=0,i<n,i++){
-    for(int j=0,j<=n,j++){
+  for(int i=0;i<n;i++){
+    for(int j=0;j<=n;j++){
     printf("%8.2f ",macierz[i][j]);
     }
     printf("\n");
